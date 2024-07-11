7c08776bf22ed512c1e9274f215e7f767f807994
PeanutDumplings <nicholas.whyte@outlook.com>
2024-07-11 13:27:54 +0930

    Refactored to use ids as keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,31 +1,32 @@
 export const Agents = Object.freeze({
-  Astra: "agent_astra",
-  Breach: "agent_breach",
-  Brimstone: "agent_brimstone",
-  Chamber: "agent_chamber",
-  Clove: "agent_clove",
-  Cypher: "agent_cypher",
-  Deadlock: "agent_deadlock",
-  Fade: "agent_fade",
-  Gekko: "agent_gekko",
-  Habor: "agent_habor",
-  Iso: "agent_iso",
-  Jett: "agent_jett",
-  Kayo: "agent_kayo",
-  Killjoy: "agent_killjoy",
-  Neon: "agent_neon",
-  Omen: "agent_omen",
-  Phoenix: "agent_phoenix",
-  Raze: "agent_raze",
-  Reyna: "agent_reyna",
-  Sage: "agent_sage",
-  Skye: "agent_skye",
-  Sova: "agent_sova",
-  Viper: "agent_viper",
-  Yoru: "agent_yoru",
+  "41fb69c1-4189-7b37-f117-bcaf1e96f1bf": "agent_astra",
+  "5f8d3a7f-467b-97f3-062c-13acf203c006": "agent_breach",
+  "9f0d8ba9-4140-b941-57d3-a7ad57c6b417": "agent_brimstone",
+  "22697a3d-45bf-8dd7-4fec-84a9e28c69d7": "agent_chamber",
+  "1dbf2edd-4729-0984-3115-daa5eed44993": "agent_clove",
+  "117ed9e3-49f3-6512-3ccf-0cada7e3823b": "agent_cypher",
+  "cc8b64c8-4b25-4ff9-6e7f-37b4da43d235": "agent_deadlock",
+  "dade69b4-4f5a-8528-247b-219e5a1facd6": "agent_fade",
+  "e370fa57-4757-3604-3648-499e1f642d3f": "agent_gekko",
+  "95b78ed7-4637-86d9-7e41-71ba8c293152": "agent_habor",
+  "0e38b510-41a8-5780-5e8f-568b2a4f2d6c": "agent_iso",
+  "add6443a-41bd-e414-f6ad-e58d267f4e95": "agent_jett",
+  "601dbbe7-43ce-be57-2a40-4abd24953621": "agent_kayo",
+  "1e58de9c-4950-5125-93e9-a0aee9f98746": "agent_killjoy",
+  "bb2a4828-46eb-8cd1-e765-15848195d751": "agent_neon",
+  "8e253930-4c05-31dd-1b6c-968525494517": "agent_omen",
+  "eb93336a-449b-9c1b-0a54-a891f7921d69": "agent_phoenix",
+  "f94c3b30-42be-e959-889c-5aa313dba261": "agent_raze",
+  "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc": "agent_reyna",
+  "569fdd95-4d10-43ab-ca70-79becc718b46": "agent_sage",
+  "6f2a04ca-43e0-be17-7f36-b3908627744d": "agent_skye",
+  "320b2a48-4d9b-a075-30f1-1f93a9b638fa": "agent_sova",
+  "707eab51-4836-f488-046a-cda6bf494859": "agent_viper",
+  "7f94d92c-4234-0a36-9646-3a87eb8b5c89": "agent_yoru",
 });
 
-export const StandardMaps = Object.freeze({
+export const Maps = Object.freeze({
+  // Standard Maps
   "/Game/Maps/Infinity/Infinity": "splash_abyss_square",
   "/Game/Maps/Ascent/Ascent": "splash_ascent_square",
   "/Game/Maps/Duality/Duality": "splash_bind_square",
@@ -37,15 +38,13 @@ export const StandardMaps = Object.freeze({
   "/Game/Maps/Pitt/Pitt": "splash_pearl_square",
   "/Game/Maps/Bonsai/Bonsai": "splash_split_square",
   "/Game/Maps/Juliett/Juliett": "splash_sunset_square",
-});
-
-export const PracticeMaps = Object.freeze({
-  "/Game/Maps/Poveglia/Range": "splash_range_square",
-});
-
-export const TeamDeathmatchMaps = Object.freeze({
+  // Team Deathmatch Maps
   "/Game/Maps/HURM/HURM_Alley/HURM_Alley": "splash_district_square",
   "/Game/Maps/HURM/HURM_Helix/HURM_Helix": "splash_drift_square",
   "/Game/Maps/HURM/HURM_Bowl/HURM_Bowl": "splash_kasbah_square",
   "/Game/Maps/HURM/HURM_Yard/HURM_Yard": "splash_piazza_square",
+});
+
+export const PracticeMaps = Object.freeze({
+  "/Game/Maps/Poveglia/Range": "splash_range_square",
 });
